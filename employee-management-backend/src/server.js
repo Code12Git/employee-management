357f07361c7e5120f8790c4a6484f47cc42a0ae7
saxenasaksham46 <saxenasaksham46>
2025-06-15 09:49:45 +0530

    Added redirects

--- employee-management-backend/src/server.js
+++ employee-management-backend/src/server.js
@@ -29,12 +29,6 @@ app.use('/api/v1/employee', employeeRoute)
 app.use('/api/v1/attendance', attendanceRoute)
 app.use('/api/v1/payroll', payrollRoute)
 
-app.use(express.static(path.join(__dirname, '../../employee-management-client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../employee-management-client/dist', 'index.html'));
-});
-
 app.use((req, res, next) => {
     logger.info({
         method: req.method,
